c9dfb44828fd491e08aece697da6bb416adf1cd0
Volodymyr Opolskyi <vovaoplolski@gmail.com>
2026-06-08 15:16:59 +0200

    feat: Reka obsluguje asa jako 11 lub 1 (zielony)

--- src/main/java/blackjack/model/Reka.java
+++ src/main/java/blackjack/model/Reka.java
@@ -13,9 +13,21 @@ public class Reka {
 
     public int getScore() {
         int total = 0;
+
+        int aceCount = 0;
+
         for (Card card : cards) {
             total += card.getValue();
+            if (card.getRank() == Rank.ACE) {
+                aceCount++;
+            }
         }
+
+        while (total > 21 && aceCount > 0) {
+            total -= 10;
+            aceCount--;
+        }
+
         return total;
     }
 
