1482eebcf2a46a3098c2043ee0f5c35438e91d68
Volodymyr Opolskyi <vovaoplolski@gmail.com>
2026-06-08 15:07:49 +0200

    feat: Reka zwraca 0 dla pustej reki (zielony)

---
+++ src/main/java/blackjack/model/Reka.java
@@ -0,0 +1,14 @@
+package blackjack.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reka {
+
+    private final List<Card> cards = new ArrayList<>();
+
+    public int getScore() {
+        return 0;
+    }
+
+}
