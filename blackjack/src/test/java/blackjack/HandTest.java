08bc75f903436744c431f9175d21f613e9de9658
Volodymy Opolskyi <vovaoplolski@gmail.com>
2026-06-15 15:28:39 +0200

    test: hand calculates score without ace (red)

--- blackjack/src/test/java/blackjack/HandTest.java
+++ blackjack/src/test/java/blackjack/HandTest.java
@@ -11,4 +11,12 @@ class HandTest {
         Hand hand = new Hand();
         assertEquals(0, hand.getScore());
     }
+
+    @Test
+    void handCalculatesScoreWithoutAce() {
+        Hand hand = new Hand();
+        hand.addCard(new Card(Suit.SPADES, Rank.KING));
+        hand.addCard(new Card(Suit.HEARTS, Rank.EIGHT));
+        assertEquals(18, hand.getScore());
+    }
 }
