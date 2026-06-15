626287ae15f5bc1436870616e01a0f5e277e9ca4
Volodymy Opolskyi <vovaoplolski@gmail.com>
2026-06-15 15:26:12 +0200

    feat: Hand skeleton with getScore returning 0

---
+++ blackjack/src/main/java/blackjack/model/Hand.java
@@ -0,0 +1,12 @@
+package blackjack.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+    private final List<Card> cards = new ArrayList<>();
+
+    public int getScore() {
+        return 0;
+    }
+}
