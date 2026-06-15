dd4d8380a9b7890712daec671f18c44d2ed029e9
Volodymy Opolskyi <vovaoplolski@gmail.com>
2026-06-15 15:28:08 +0200

    feat: Hand sums card values

--- blackjack/src/main/java/blackjack/model/Hand.java
+++ blackjack/src/main/java/blackjack/model/Hand.java
@@ -6,7 +6,15 @@ import java.util.List;
 public class Hand {
     private final List<Card> cards = new ArrayList<>();
 
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
     public int getScore() {
-        return 0;
+        int total = 0;
+        for (Card card : cards) {
+            total += card.getValue();
+        }
+        return total;
     }
 }
