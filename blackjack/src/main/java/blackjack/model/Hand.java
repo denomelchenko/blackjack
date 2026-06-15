1285793ca867f29ae4fb5b2f53cd629a0d5d0c8d
Volodymy Opolskyi <vovaoplolski@gmail.com>
2026-06-15 15:30:46 +0200

    feat: Hand handles ace as 11 or 1

--- blackjack/src/main/java/blackjack/model/Hand.java
+++ blackjack/src/main/java/blackjack/model/Hand.java
@@ -12,9 +12,20 @@ public class Hand {
 
     public int getScore() {
         int total = 0;
+        int aceCount = 0;
+
         for (Card card : cards) {
             total += card.getValue();
+            if (card.getRank() == Rank.ACE) {
+                aceCount++;
+            }
         }
+
+        while (total > 21 && aceCount > 0) {
+            total -= 10;
+            aceCount--;
+        }
+
         return total;
     }
 }
